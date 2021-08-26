86396cd73c395daa672f757d13b5f1df52b8c278
Lívia Barbosa <liviaab7@gmail.com>
2021-08-26 19:02:06 -0300

    Update setup description

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setup(
     version="0.1",
     packages=find_packages(),
     author="Lívia Almeida Barbosa",
-    description="This project is the backend of an application used to "
-                "detect commits that migrate unittest to pytest in Python repositories",
+    description="Application to detect commits that migrate unittest to pytest in Python repositories",
     url="https://github.com/liviaab/tfmig",
 )
